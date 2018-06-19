cada67305e16e4a4507551e45e0996544cd43c38
Sundeep B <bhusu01@ca.com>
2018-06-19 18:02:49 +0200

    Initial commit spring app

---
+++ src/main/java/com/demo/ticker/tickerapp/TickerappApplication.java
@@ -0,0 +1,12 @@
+package com.demo.ticker.tickerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickerappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TickerappApplication.class, args);
+	}
+}
---
+++ src/test/java/com/demo/ticker/tickerapp/TickerappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.demo.ticker.tickerapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TickerappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
